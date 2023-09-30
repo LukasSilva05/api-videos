9657abcce537b3fc82174f6e8f31e8878bf5a6cd
Lukas Silva <lukasg.dasilva2014@gmail.com>
2023-09-30 13:10:26 -0300

    first push again

--- app.js
+++ app.js
@@ -4,14 +4,6 @@ const videos = require('./videos.json');
 
 app.use(cors())
 
-app.get('/', (req, res) => res.send(`
-    <h1>Rotas: <br> 
-    <a href='http://localhost:3000/js'>javascript</a> <br>
-    <a href='http://localhost:3000/html'>html</a> <br>
-    <a href='http://localhost:3000/vscode'>vscode</a>
-    </h1>
-`))
-
 app.get('/js', (req, res) => res.send(videos.js));
 
 app.get('/html', (req, res) => res.send(videos.html))
