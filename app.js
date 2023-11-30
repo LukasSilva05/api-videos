13545353a0493c58f52837713234952e897d4a15
Lukas Silva <lukasg.dasilva2014@gmail.com>
2023-11-30 17:59:39 -0300

    new configs

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const videos = require('./videos.json');
 
 app.use(cors())
 
-app.get('/js', (req, res) => res.send(videos.js));
-
-app.get('/html', (req, res) => res.send(videos.html))
-
-app.get('/vscode', (req, res) => res.send(videos.vscode))
+app.get('/', (req, res) => res.send(videos));
 
 app.listen(process.env.PORT || 3000, () => console.log('servidor aberto em http://localhost:3000'))
